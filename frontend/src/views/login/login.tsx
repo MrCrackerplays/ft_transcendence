fbfbe6e1d606fd604b8ce229bf72ab2cfc0de59b
Nick Gerrets <ngerrets@student.codam.nl>
2023-05-08 15:08:49 +0200

    constants TS file

--- frontend/src/views/login/login.tsx
+++ frontend/src/views/login/login.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Router, redirect } from 'react-router-dom';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import * as Constants from "../../../../shared/constants";
 import './login.css'
 
 function MyLoginPage()
 {
-  const link = "http://localhost:3000/login"
+  const link = Constants.BACKEND_LOGIN_REDIRECT
   return (
     <div className="login">
 		  <h1 className="text"> So, You're Trying to Roll into Ball Busters huh?</h1>
---
+++ shared/constants.ts
@@ -0,0 +1,14 @@
+export const BACKEND_HOST_PREFIX = 'http://';
+export const BACKEND_HOST = 'localhost';
+export const BACKEND_PORT = '3000';
+
+export const FRONTEND_HOST_PREFIX = 'http://';
+export const FRONTEND_HOST = 'localhost';
+export const FRONTEND_PORT = '5173';
+
+export const BACKEND_URL = `${BACKEND_HOST_PREFIX}${BACKEND_HOST}:${BACKEND_PORT}`;
+export const FRONTEND_URL = `${FRONTEND_HOST_PREFIX}${FRONTEND_HOST}:${FRONTEND_PORT}`;
+
+export const BACKEND_LOGIN_REDIRECT = `${BACKEND_URL}/login`;
+export const FRONTEND_LOGIN_REDIRECT = `${FRONTEND_URL}/login`;
+
