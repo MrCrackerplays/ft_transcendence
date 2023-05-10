5458d4384242b6bae9e50f73d9953af2c0d00d72
Zachary Najda <znajda@f1r4s20.codam.nl>
2023-05-10 17:50:46 +0200

    shhh

--- frontend/src/hooks/navbar/navbar.tsx
+++ frontend/src/hooks/navbar/navbar.tsx
@@ -3,11 +3,11 @@ import MyPopover from "./play";
 import './home.css'
 import { AppBar, Toolbar, Typography, Button, Popover } from '@mui/material';
 
-function MyNavBar( {name} )
+function MyNavBar( {name, imgsrc} )
 {
 	return (
 		<div className="my-navbar">
-			<MyPopover name={name}/>
+			<MyPopover name={name} imgsrc={imgsrc}/>
 		</div>
 	)
 }
