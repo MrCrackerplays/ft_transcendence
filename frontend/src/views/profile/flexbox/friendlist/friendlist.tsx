8b9d2c06272778536f81d61745b14859dcd9d053
Zachary Najda <znajda@f1r4s17.codam.nl>
2023-06-15 15:10:06 +0200

    adding frontend changes to friendlist instead of array using single

--- frontend/src/views/profile/flexbox/friendlist/friendlist.tsx
+++ frontend/src/views/profile/flexbox/friendlist/friendlist.tsx
@@ -7,12 +7,31 @@ import { Constants } from "../../../../../../shared/constants";
 
 import UserStatus from "../../../../hooks/userStatus/userStatus";
 
-function FriendCard({ friend }: {friend: PublicUser}){
+function FriendCard({ friend, setcurrentFriend, ownID, currentFriend }: {friend: PublicUser, setcurrentFriend:(something: {userid: string, value: boolean}) => void , ownID: string, currentFriend: {userid:string, value:boolean}})
+{
 	const textcolor = "white";
 	const fetchPFP = Constants.FETCH_USERS;
-
+	const [active, setactive] = useState(false);
+	function handleMouseDown()
+	{
+		if (ownID == currentFriend.userid)
+			setcurrentFriend({...currentFriend, value: !currentFriend.value});
+		else
+			setcurrentFriend({userid: ownID, value: true});
+	}
+	const test = () => {
+		console.log("clickity");
+		if (ownID == currentFriend.userid)
+		{
+			if (currentFriend.value == true)
+				return ("greenfriend");
+			// return (currentFriend.value)
+		}
+		return ("redfriend")
+	}
+	const action = test();
 	return (
-		<div className="friend-card">
+		<div className={`friend-card ${action}`} onMouseDown={handleMouseDown}>
 			<img src={`${fetchPFP}/${friend.userName}/pfp`} alt="pfp not found" />
 			<p className={friend.status}>{friend.userName}</p>
 		</div>
@@ -21,7 +40,7 @@ function FriendCard({ friend }: {friend: PublicUser}){
 
 function MyFriendsList() {
 	const [friendArray, setFriendArray] = useState<PublicUser[]>([]);
-	const [useStateArray, setStateArray] = useState<boolean[]>([]);
+	const [currentFriend, setcurrentFriend] = useState({userid: "", value: false})
 	const [isLoading, setisLoading] = useState(true);
 	
 	async function getFriends() {
@@ -43,8 +62,8 @@ function MyFriendsList() {
 	return (
 		<div className="all-friends">
 			{
-				friendArray.map((friend, id) => (
-					<FriendCard key={friend.id} friend={friend} setStateArray={setStateArray} ownNumber={id} useStateArray={useStateArray}/>
+				friendArray.map((friend) => (
+					<FriendCard key={friend.id} friend={friend} setcurrentFriend={setcurrentFriend} ownID={friend.id} currentFriend={currentFriend}/>
 				))
 			}
 		</div>
