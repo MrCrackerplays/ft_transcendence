aa8b48750c00bf263278dc0dd5b39bf36d3e4769
Yuliia <109362182+ydemura@users.noreply.github.com>
2023-06-17 18:06:59 +0200

    local working raw

--- frontend/src/hooks/game/pong.tsx
+++ frontend/src/hooks/game/pong.tsx
@@ -5,12 +5,12 @@ import "./pong.css";
 
 //this - on BE only?
 type Players = {
-	PlayerScore : number,
-	OpponentScore : number,
-	PlayerID : string,
-	OpponentID : string,
-	PlayerWin : boolean,
-	OpponentWin : boolean,
+	PlayerScore: number,
+	OpponentScore: number,
+	PlayerID: string,
+	OpponentID: string,
+	PlayerWin: boolean,
+	OpponentWin: boolean,
 };
 
 enum PaddleAction {
@@ -43,15 +43,19 @@ type GameState = {
 	rightPaddle: PaddleState,
 	ball: BallState,
 	time: number,
+	gameOver: boolean,
+	winner: string,
 };
 
-const paddleHeight = 0.15;
+const paddleHeight = 0.3;
 const paddleWidth = 0.02;
-const paddleSpeed = 1;
-const ballHeight = 0.02;
-const ballWidth = 0.02;
+const framePaddleGap = 0.03;
+const paddleSpeed = 0.5;
+const ballHeight = 0.04;
+const ballWidth = 0.04;
+const timeDlta = 0.02;
 
-function checkPaddleBoarder(paddlePosition) {
+function checkPaddleBoarder(paddlePosition) {//up and down boader frame for paddle
 	const paddleMaxUp = 1 - paddleHeight / 2;
 	const paddleMaxDown = -1 + paddleHeight / 2;
 	if (paddlePosition >= paddleMaxUp) {
@@ -69,7 +73,6 @@ function updateBallPosition(state: GameState, timeDlta: number) {
 }
 
 function updateBall(state: GameState, timeDlta: number) {
-
 	const ball = state.ball;
 	const leftPaddle = state.leftPaddle;
 	const rightPaddle = state.rightPaddle;
@@ -79,33 +82,34 @@ function updateBall(state: GameState, timeDlta: number) {
 
 	//up and down wall collision
 	if (ball.position.y >= maxBallPosition || ball.position.y <= minBallPosition) {
-		state.ball.velocity = { x : ball.velocity.x, y : (ball.velocity.y * -1) };
+		state.ball.velocity = { x: ball.velocity.x, y: (ball.velocity.y * -1) };
 		return;
-	} 
+	}
 
+	const padX = paddleWidth + framePaddleGap * 2;//why * 2? dunno
 
-	const padStartX = paddleWidth * 2;
-	const padEndX = paddleWidth * 2;
-	
-	//padle collision
-	//paddle position is middle of paddle
+	const minBallY = ball.position.y - (ballHeight / 2);
+	const maxBallY = ball.position.y + (ballHeight / 2);
+
+	//padle collision : left
 	const leftMaxPaddleY = (leftPaddle.paddlePosition + paddleHeight / 2);
 	const leftMinPaddleY = (leftPaddle.paddlePosition - paddleHeight / 2);
-	
+	const isBallAtLeftWall = (ball.position.x - ballWidth / 1) - padX <= -1;
+	const willBounceLeftPaddle = (leftMinPaddleY <= minBallY && minBallY <= leftMaxPaddleY) || (leftMinPaddleY <= maxBallY && maxBallY <= leftMaxPaddleY);
+
+	//padle collision : right
 	const rightMaxPaddleY = (rightPaddle.paddlePosition + paddleHeight / 2);
 	const rightMinPaddleY = (rightPaddle.paddlePosition - paddleHeight / 2);
-	
-	const isBallAtLeftWall = (ball.position.x - ballWidth / 2) <= -1;
-	const isBallAtRightWall = (ball.position.x + ballWidth / 2) >= 1;
-	const willBounceLeftPaddle = leftMinPaddleY <= ball.position.y && ball.position.y <= leftMaxPaddleY;
-	const willBounceRightPaddle = rightMinPaddleY <= ball.position.y && ball.position.y <= rightMaxPaddleY;
+	const isBallAtRightWall = (ball.position.x + ballWidth / 1) + padX >= 1;
+	const willBounceRightPaddle = (rightMinPaddleY <= minBallY && minBallY <= rightMaxPaddleY) || (rightMinPaddleY <= maxBallY && maxBallY <= rightMaxPaddleY);;
+
 	if (isBallAtLeftWall) {
 		if (willBounceLeftPaddle) {
-			state.ball.velocity = { x : -ball.velocity.x, y : ball.velocity.y };
+			state.ball.velocity = { x: -ball.velocity.x, y: ball.velocity.y };
 			updateBallPosition(state, timeDlta);
 		} else {
-			state.ball.position = { x : 0, y : 0 };
-			state.leftPaddle.score += 1;
+			state.ball.position = { x: 0, y: 0 };
+			state.rightPaddle.score += 1;
 		}
 	} else if (isBallAtRightWall) {
 		if (willBounceRightPaddle) {
@@ -113,18 +117,28 @@ function updateBall(state: GameState, timeDlta: number) {
 			updateBallPosition(state, timeDlta);
 		} else {
 			state.ball.position = { x : 0, y : 0 };
-			state.rightPaddle.score += 1;
+			state.leftPaddle.score += 1;
 		}
-
+		//debug prps - all wall is paddle
+		state.ball.velocity = { x: -ball.velocity.x, y: ball.velocity.y };
+		updateBallPosition(state, timeDlta);
+	}
+	if (state.leftPaddle.score == 10) {
+		state.gameOver = true;
+		state.winner = state.leftPaddle.playerID;
+	} else if (state.rightPaddle.score == 10) {
+		state.gameOver = true;
+		state.winner = state.rightPaddle.playerID;
 	}
 };
 
-
-
-
 const makeReducer = (playerID: string) => {
+	
 	const reducer = (state: GameState, action: GameAction) => {
 		let newState = structuredClone(state);
+		if (newState.gameOver) {
+			return newState;
+		}
 		switch (action) {
 			case GameAction.arrowUp:
 				if (newState.leftPaddle.playerID == playerID) {
@@ -148,24 +162,23 @@ const makeReducer = (playerID: string) => {
 				}
 				break;
 			case GameAction.updateTime:
-				const timeDlta = 0.1;
 				newState.time += timeDlta;
 				switch (newState.leftPaddle.action) {
 					case PaddleAction.Up:
-						newState.leftPaddle.paddlePosition += timeDlta * paddleSpeed;
+						newState.leftPaddle.paddlePosition -= timeDlta * paddleSpeed;
 						break;
 					case PaddleAction.Down:
-						newState.leftPaddle.paddlePosition -= timeDlta * paddleSpeed;
+						newState.leftPaddle.paddlePosition += timeDlta * paddleSpeed;
 						break;
 					case PaddleAction.None:
 						break;
 				}
 				switch (newState.rightPaddle.action) {
 					case PaddleAction.Up:
-						newState.rightPaddle.paddlePosition += timeDlta * paddleSpeed;
+						newState.rightPaddle.paddlePosition -= timeDlta * paddleSpeed;
 						break;
 					case PaddleAction.Down:
-						newState.rightPaddle.paddlePosition -= timeDlta * paddleSpeed;
+						newState.rightPaddle.paddlePosition += timeDlta * paddleSpeed;
 						break;
 					case PaddleAction.None:
 						break;
@@ -187,10 +200,12 @@ const PongGame = () => {
 	const opponentID = "player2";
 
 	const initialState: GameState = {
-		leftPaddle: { playerID: playerID, paddlePosition: 0, action: PaddleAction.None, score: 0},
-		rightPaddle: { playerID: opponentID, paddlePosition: 0, action: PaddleAction.None, score: 0},
-		ball: { velocity: { x: -0.1, y: -0.001 }, position: { x: 0, y: 0 } },
+		leftPaddle: { playerID: playerID, paddlePosition: 0, action: PaddleAction.None, score: 0 },
+		rightPaddle: { playerID: opponentID, paddlePosition: 0, action: PaddleAction.None, score: 0 },
+		ball: { velocity: { x: -0.3, y: 0.3 }, position: { x: 0, y: 0 }},
 		time: 0,
+		gameOver: false,
+		winner: "",
 	};
 	const [state, dispatch] = useReducer(makeReducer(playerID), initialState);
 
@@ -220,19 +235,18 @@ const PongGame = () => {
 	useEffect(() => {
 		const interval = setInterval(() => {
 			dispatch(GameAction.updateTime);
-		}, 50); //every 50ms ball moves
+		}, timeDlta * 1000);
 		return () => clearInterval(interval);
 	}, []);
 
+	const lPaddle = 0.5 - paddleHeight / 4;
+	const kPaddle = lPaddle / (1 - paddleHeight / 2);
 
-	const k = -42.5 / 0.925;
-	const l = 42.5;
+	const leftPaddleTop = kPaddle * state.leftPaddle.paddlePosition + lPaddle;
+	const rightPaddleTop = kPaddle * state.rightPaddle.paddlePosition + lPaddle;
 
-	const lBallCorrection = 0.5 - ballWidth / 2;
+	const lBallCorrection = 0.5 - ballWidth / 4;
 	const kBallCorrection = lBallCorrection / (1 - ballWidth / 2);
-
-	const leftPaddleTop = k * state.leftPaddle.paddlePosition + l;
-	const rightPaddleTop = k * state.rightPaddle.paddlePosition + l;
 
 	const ballPositionLeft = kBallCorrection * state.ball.position.x + lBallCorrection;
 	const ballPositionTop = kBallCorrection * state.ball.position.y + lBallCorrection;
@@ -240,21 +254,43 @@ const PongGame = () => {
 	return (
 		<div className="pong-frame">
 			<div className="centre-line"></div>
-			<div className="h-centre-line"></div>
+			<div className="h-centre-line"></div>{/* REMOVE later */}
 			<div className="paddle-right" style={{
-				top: rightPaddleTop + '%',
-				height: (paddleHeight * 100) + '%',
+				//paddle right
+				top: (rightPaddleTop * 100) + '%',
+				height: (paddleHeight * 50) + '%',
+				width: (paddleWidth * 100) + '%',
+				right: (framePaddleGap * 100) + '%'
 			}}></div>
 			<div className="paddle-left" style={{
-				top: leftPaddleTop + '%',
-				height: (paddleHeight * 100) + '%',
-			}}></div>
+				//paddle left
+				top: (leftPaddleTop * 100) + '%',
+				height: (paddleHeight * 50) + '%',
+				width: (paddleWidth * 100) + '%',
+				left: (framePaddleGap * 100) + '%'
+			}}>{state.leftPaddle.paddlePosition}</div>
 			<div className="pong-ball" style={{
 				left: (ballPositionLeft * 100) + '%',
 				top: (ballPositionTop * 100) + '%',
-				width: (ballWidth * 100) + '%',
-				height: (ballHeight * 100) + '%',
-				}}>{state.ball.position.x}, {state.ball.position.y}</div>
+				width: (ballWidth * 50) + '%',
+				height: (ballHeight * 50) + '%',
+			}}></div>
+			<div className="score" style={{
+
+				top: 1 + '%',
+				left: 25 + '%',
+				width: 20 + '%',
+				height: 20 + '%',
+
+			}}>{state.leftPaddle.score}</div>
+			<div className="score" style={{
+
+				top: 1 + '%',
+				left: 75 + '%',
+				width: 20 + '%',
+				height: 20 + '%',
+
+			}}>{state.rightPaddle.score}</div>
 		</div>
 	);
 
